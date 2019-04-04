e9252724b621643b6d6b975e578d9a55d23abb94
Jonathan Hecl <jonathanhecl@gmail.com>
2019-04-03 23:01:11 -0300

    React Client

---
+++ public/precache-manifest.322cc1a0981814470fa95698ad009295.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b45352816a04acb534c",
+    "url": "/static/js/main.ef6f36c7.chunk.js"
+  },
+  {
+    "revision": "c9934e8f8ddb7edec47a",
+    "url": "/static/js/2.3cd3ed05.chunk.js"
+  },
+  {
+    "revision": "2b45352816a04acb534c",
+    "url": "/static/css/main.e344f020.chunk.css"
+  },
+  {
+    "revision": "06c8a5df0b6f57b6c1768c28411f9e89",
+    "url": "/index.html"
+  }
+];
